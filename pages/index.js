349c8a298786bd5ffadc4191ff5659bcbd51e4cc
calebbabin <object.caleb@gmail.com>
2022-05-05 09:02:53 -0500

    update title/description

--- pages/index.js
+++ pages/index.js
@@ -15,32 +15,11 @@ export default function Home() {
 			<Metadata title={title} />
 
 			<Section>
-
 				<Container>
 					<GenericContent>
-						<h1>{title}</h1>
-						<p>{text}</p>
-
 						<HomeContent />
 					</GenericContent>
 				</Container>
-
-
-				<Container className='theme-dark'>
-					<h2>About</h2>
-					<p>
-						Further information about this project can be found online.
-					</p>
-				</Container>
-
-
-				<Container className='theme-light'>
-					<h2>Cool alternating section</h2>
-					<p>
-						Alternative text inside this alternated section.
-					</p>
-				</Container>
-
 			</Section>
 
 		</Layout>
