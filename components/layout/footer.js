56c5bbe7f4b4f1b9a3e43ea1b4961b8d5f921efd
calebbabin <object.caleb@gmail.com>
2022-05-12 16:55:50 -0500

    decrease footer margin

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import GenericContent from "../genericContent";
 
 const Footer = () => {
-	return (<div className="mt-20 bg-gray-800 p-10">
+	return (<div className="mt-10 bg-gray-800 p-10">
 		<nav className="max-w-7xl mx-auto w-full px-2 py-8 box-border flex justify-between">
 			<GenericContent>
 				<b>Holaplex</b>
