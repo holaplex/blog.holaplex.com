622192e05a891e6673e57cc17962eeb07d4006ae
calebbabin <object.caleb@gmail.com>
2022-05-11 20:47:27 -0500

    use GenericContent component

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,16 +1,17 @@
 import React from "react";
+import GenericContent from "../genericContent";
 
 const Footer = () => {
 	return (<div className="mt-20 bg-gray-800 p-10">
 		<nav className="max-w-7xl mx-auto w-full px-2 py-8 box-border flex justify-between">
-			<div>
+			<GenericContent>
 				<b>Holaplex</b>
 				<br />
 				©️ {new Date().getFullYear()}
 				<p>
 					The only truly open-sourced, decentralized &amp; community-governed NFT platform on Solana.
 				</p>
-			</div>
+			</GenericContent>
 		</nav>
 	</div>);
 };
