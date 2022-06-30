c6017b94e035f2c0dbc2d1d5c551006a64bb1679
calebbabin <object.caleb@gmail.com>
2022-06-30 16:45:34 -0500

    add newsletter form to blog footer

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,14 +1,12 @@
 import React from "react";
 import GenericContent from "../genericContent";
+import NewsletterForm from "../newsletter-form";
 
 const Footer = () => {
-	return (<div className="mt-10 bg-gray-800 p-10">
-		<nav className="max-w-7xl mx-auto w-full px-2 py-8 box-border flex justify-between">
-			<GenericContent>
-				<b>Holaplex</b>
-				<br />
-				©️ {new Date().getFullYear()}
-			</GenericContent>
+	return (<div className="mt-10 p-10">
+		<nav className="max-w-7xl mx-auto w-full px-2 py-8 box-border flex flex-col justify-center items-center text-center">
+			<p className="my-4">Stay up to date with Holaplex</p>
+			<NewsletterForm />
 		</nav>
 	</div>);
 };
